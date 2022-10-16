cfba738927117ecf29189671e6235afbfba90f09
GYHHAHA <1801214626@qq.com>
2022-10-16 14:47:39 -0500

    fix impute method

--- code/clean_data.py
+++ code/clean_data.py
@@ -7,14 +7,16 @@ if __name__ == "__main__":
     # Compute the error between Siri density and actual density
     siri_density = 495 / (df.BODYFAT + 450)
     err = siri_density - df.DENSITY
-    # Use 3 sigma criterion to drop the abnormal samples
+    # Use 3 sigma criterion to find the abnormal samples
     mean_err = err.mean()
     std_err = 3 * err.std()
     err_id = df[(err > mean_err + std_err) | (err < mean_err - std_err)].IDNO
-    df = df[~df.IDNO.isin(err_id)]
+    # Use the density to impute the body fat
+    df.loc[err_id, "BODYFAT"] = (495 / df.loc[err_id, "DENSITY"] - 450).round(1)
     # Drop the samples with negative bodyfat by Siri density
     siri_bodyfat = 495 / df.DENSITY - 450
     negative_id = df[siri_bodyfat < 0].IDNO
     df = df[~df.IDNO.isin(negative_id)]
     # Reset index and save data
-    df.reset_index(drop=True).to_csv("../data/cleaned_data.csv", index=False)
+    df = df.drop(["IDNO", "DENSITY"], axis=1).reset_index(drop=True)
+    df.to_csv("../data/cleaned_data.csv", index=False)
